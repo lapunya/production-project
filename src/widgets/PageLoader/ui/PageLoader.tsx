4ed1da17629d6746138b0dcb8038ddc81bac7249
Pavel Lapunov <pavel.lapunov@quantron-systems.com>
2025-09-08 14:02:40 +0300

    add PageLoader widget

---
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -0,0 +1,18 @@
+import {classNames} from 'shared/lib/classNames/classNames';
+import cls from './PageLoader.module.scss';
+import {FC} from 'react';
+import { Loader } from 'shared/ui/Loader/Loader';
+
+interface PageLoaderProps {
+ className?: string;
+}
+
+export const PageLoader:FC<PageLoaderProps> = (props) => {
+    const {className} = props;
+
+    return (
+        <div className={classNames(cls.pageLoader, {}, [className])}>
+            <Loader/>
+        </div>
+    );
+}
